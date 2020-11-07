aff2e71a548516a506110d601aae319b616fa8be
thanh tien <thanhhvt7196@gmail.com>
2020-11-07 16:04:35 +0700

    initial project

---
+++ SwiftUIAniamtion/SwiftUIAniamtion/SwiftUIAniamtionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAniamtionApp.swift
+//  SwiftUIAniamtion
+//
+//  Created by thanh tien on 11/7/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAniamtionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
